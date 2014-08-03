6bdb680dce7eb8a0f32baa6f7d3225629d6d60ec
Psyka <psyka@Psykas-MacBook-Pro.local>
2014-08-03 06:31:29 +0200

    Choice of port added

--- server_d/main.cpp
+++ server_d/main.cpp
@@ -1,9 +1,17 @@
 #include "mygame.h"
 
-int main()
+int 	main()
 {
-    server     my_serv(4242);
+	int		port;
 
+	cout << "Veuillez entrer un port entre 1024 et 49151 : " << endl;
+	cin >> port;
+	if (port < 1024 || port > 49151)
+	{
+		cerr << "Server : error wrong port." << endl;
+		exit (0);
+	}
+    server my_serv(port);
     cout << "Server started." << endl;
     while (my_serv.run_serv() == 1)
         ;
